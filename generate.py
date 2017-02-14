b33d70123d60eba9d06a23d076aa5392c67d4e18
Daice <zmzkee@gmail.com>
2017-02-14 20:22:45 +0800

    fix bug

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import sugartensor as tf
+import matplotlib
+matplotlib.use('Agg') # Force matplotlib to not use any Xwindows backend.
 import matplotlib.pyplot as plt
 
 __author__ = 'buriburisuri@gmail.com'
@@ -57,7 +59,7 @@ with tf.Session() as sess:
 
         # restore parameters
         saver = tf.train.Saver()
-        saver.restore(sess, tf.train.latest_checkpoint('asset/train/ckpt'))
+        saver.restore(sess, tf.train.latest_checkpoint('asset/train'))
 
         # run generator
         gt, low, bicubic, sr = sess.run([x.sg_squeeze(), x_nearest, x_bicubic, gen])
